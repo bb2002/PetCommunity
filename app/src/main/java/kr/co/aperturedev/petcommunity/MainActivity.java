fe01ef56bd420ddc066285890f5febdf37b33ad2
kyongkyong2 <kyongkyong289@gmail.com>
2018-02-28 11:07:49 +0900

    123

--- app/src/main/java/kr/co/aperturedev/petcommunity/MainActivity.java
+++ app/src/main/java/kr/co/aperturedev/petcommunity/MainActivity.java
@@ -1,5 +1,8 @@
 package kr.co.aperturedev.petcommunity;
 
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,MainPage.class);
+                startActivity(intent);
+            }
+        },2000);
     }
 }
