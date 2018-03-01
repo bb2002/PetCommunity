77856981c64802c418a05cc1fa79010b23f57fbd
kyongkyong2 <kyongkyong289@gmail.com>
2018-03-01 15:43:01 +0900

    123

--- app/src/main/java/kr/co/aperturedev/petcommunity/view/activitys/GetInformationActivity.java
+++ app/src/main/java/kr/co/aperturedev/petcommunity/view/activitys/GetInformationActivity.java
@@ -3,13 +3,11 @@ package kr.co.aperturedev.petcommunity.view.activitys;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import kr.co.aperturedev.petcommunity.R;
-
-public class CoupleMatchingActivity extends AppCompatActivity {
+public class GetInformationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_couple_matching);
+        setContentView(R.layout.activity_get_information);
     }
 }
