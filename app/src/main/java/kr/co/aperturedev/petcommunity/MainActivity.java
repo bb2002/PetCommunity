9a6da13d5542e23a9cb5696b1cc60505c7c2934c
kyongkyong2 <kyongkyong289@gmail.com>
2018-03-02 15:31:41 +0900

    123

--- app/src/main/java/kr/co/aperturedev/petcommunity/MainActivity.java
+++ app/src/main/java/kr/co/aperturedev/petcommunity/MainActivity.java
@@ -9,20 +9,39 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.webkit.WebView;
+import android.widget.TextView;
 import android.widget.ViewFlipper;
 
 import kr.co.aperturedev.petcommunity.view.activitys.DiaryActivity;
 import kr.co.aperturedev.petcommunity.view.activitys.GetInformationActivity;
 import kr.co.aperturedev.petcommunity.view.activitys.MatchingActivity;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
     ViewFlipper viewFlipper;
     WebView webView;
-
+    TextView board1View;
+    Document board1;
+    Elements element1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_main2);
+        try {
+            board1 = Jsoup.connect("http://www.goldntree.co.kr/board/free/list.html?board_no=1").get();
+            element1 = board1.select("span.subject");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        board1View = (TextView)findViewById(R.id.mainNumber);
+        board1View.setText(element1.text());
         //뷰플리퍼실행
         viewFlipper = (ViewFlipper)findViewById(R.id.bannerView);
         viewFlipper.setFlipInterval(3000);
@@ -60,10 +79,6 @@ public class MainActivity extends AppCompatActivity {
     }
     public void banner3Click(View v) {
         //세번째배너클릭
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.goldntree.co.kr/board/free/list.html?board_no=1"));
-        startActivity(intent);//웹띄우기
-    }
-    public void textClick(View v) {
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.goldntree.co.kr/board/free/list.html?board_no=1"));
         startActivity(intent);//웹띄우기
     }
