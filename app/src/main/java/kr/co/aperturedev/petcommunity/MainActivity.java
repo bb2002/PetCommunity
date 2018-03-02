1da28f873c6ba42b97b520d69455639cb04bc979
kyongkyong2 <kyongkyong289@gmail.com>
2018-03-02 12:24:33 +0900

    123

--- app/src/main/java/kr/co/aperturedev/petcommunity/MainActivity.java
+++ app/src/main/java/kr/co/aperturedev/petcommunity/MainActivity.java
@@ -7,17 +7,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ViewFlipper;
 
 import kr.co.aperturedev.petcommunity.view.activitys.DiaryActivity;
 import kr.co.aperturedev.petcommunity.view.activitys.GetInformationActivity;
 import kr.co.aperturedev.petcommunity.view.activitys.MatchingActivity;
 
 public class MainActivity extends AppCompatActivity {
+    ViewFlipper viewFlipper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_main2);
+        viewFlipper = (ViewFlipper)findViewById(R.id.bannerView);
+        viewFlipper.setFlipInterval(1500);
+        viewFlipper.startFlipping();
     }
 
     public void goMatching(View v) {
