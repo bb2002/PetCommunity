cb8a7fd46a7974516ea79a2bf592be7c54d9af96
kyongkyong2 <kyongkyong289@gmail.com>
2018-03-05 14:04:29 +0900

    123

--- app/src/main/java/kr/co/aperturedev/petcommunity/MainActivity.java
+++ app/src/main/java/kr/co/aperturedev/petcommunity/MainActivity.java
@@ -2,6 +2,7 @@ package kr.co.aperturedev.petcommunity;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Handler;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -35,62 +36,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_main2);
-
-        /*
-        메인스레드에서 네트워크 부분은 처리 하면 안됩니다.
-
-        try {
-            board1 = Jsoup.connect("http://www.goldntree.co.kr/board/free/list.html?board_no=1").get();
-            element1 = board1.select("td.subject");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        titles1="";
-        num=0;
-        for(Element element:element1) {
-            titles1 += num+element.text()+"\n";
-            num++;
-            if(num==10)
-                break;
-        }
-        board1View = (TextView)findViewById(R.id.mainNumber);
-        board1View.setText(titles1);
         //뷰플리퍼실행
         viewFlipper = (ViewFlipper)findViewById(R.id.bannerView);
         viewFlipper.setFlipInterval(3000);
         viewFlipper.startFlipping(); //뷰플리퍼3초간격슬라이드
-        */
     }
 
-    public void goMatching(View v) {
+    protected void goMatching(View v) {
         //매칭버튼누를때
         Intent intent = new Intent(this, MatchingActivity.class);
         startActivity(intent);
         finish();//매칭으로이동
     }
-    public void goDiary(View v) {
+    protected void goDiary(View v) {
         //다이어리버튼누를때
         Intent intent = new Intent(this, DiaryActivity.class);
         startActivity(intent);
         finish();//다이어리로이동
     }
-    public void goRecommend(View v) {
+    protected void goRecommend(View v) {
         //추천버튼누를때
         Intent intent = new Intent(this, GetInformationActivity.class);
         startActivity(intent);
         finish();//추천으로이동
     }
-    public void banner1Click(View v) {
+    protected void banner1Click(View v) {
         //첫배너클릭
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.goldntree.co.kr"));
         startActivity(intent);//웹띄우기
     }
-    public void banner2Click(View v) {
+    protected void banner2Click(View v) {
         //둘째배너클릭
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.goldntree.co.kr/board/product/list.html?board_no=6"));
         startActivity(intent);//웹띄우기
     }
-    public void banner3Click(View v) {
+    protected void banner3Click(View v) {
         //세번째배너클릭
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.goldntree.co.kr/board/free/list.html?board_no=1"));
         startActivity(intent);//웹띄우기
