b9412a5f11c38ba69ed22ac9b73fc4d89fabe3cd
Party4Bread <solsam10@naver.com>
2018-03-15 23:18:06 +0900

    사진업로드 변경
    서버를 바꾸고 전송할때 드물게 생기는 오류를 수정했습니다

--- app/src/main/java/kr/co/aperturedev/petcommunity/modules/uploader/ImageUploadTask.java
+++ app/src/main/java/kr/co/aperturedev/petcommunity/modules/uploader/ImageUploadTask.java
@@ -2,18 +2,16 @@ package kr.co.aperturedev.petcommunity.modules.uploader;
 
 import android.graphics.Bitmap;
 import android.os.AsyncTask;
-import android.util.Log;
 
-import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.InetSocketAddress;
-import java.net.Socket;
-import java.net.SocketAddress;
 import java.net.SocketException;
+
+import okhttp3.Call;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 /**
  * Bitmap -> Upload target
@@ -39,8 +37,8 @@ public class ImageUploadTask extends AsyncTask<Void, Integer, Object[]> {
         boolean isUploaded = (boolean) result[0];   // 업로드 결과
         String reasonOrLink = (String) result[1];   // isUploaed ? 'FILE LINK' : 'REASON';
 
-        if(listener != null) {
-            if(isUploaded) {
+        if (listener != null) {
+            if (isUploaded) {
                 listener.onUploaded(reasonOrLink);
             } else {
                 listener.onUploadFailed(reasonOrLink);
@@ -55,7 +53,7 @@ public class ImageUploadTask extends AsyncTask<Void, Integer, Object[]> {
     protected void onProgressUpdate(Integer... values) {
         super.onProgressUpdate(values);
 
-        if(listener != null) {
+        if (listener != null) {
             listener.onPacketSended((int) (((double) values[0] / (double) fileSize) * 100));
         }
     }
@@ -65,92 +63,48 @@ public class ImageUploadTask extends AsyncTask<Void, Integer, Object[]> {
         /*
         Bitmap image 를 byte[] 으로 바꾼다.
          */
+        onProgressUpdate(0);
         ByteArrayOutputStream stream = new ByteArrayOutputStream();
         uploadImage.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+        onProgressUpdate(10);
         byte[] byteArray = stream.toByteArray();
 
-        fileSize = byteArray.length;
-
-        // 서버에 연결한다.
-        Socket dataSocket = new Socket();   // 파일 전송 소켓
-        Socket infoSocket = new Socket();   // 정보 교환 소켓
-
-        // Socket pipe 를 만든다.
-        DataOutputStream infoDos = null;	// Client -> Server [info]
-        DataInputStream infoDis = null;		// Server -> Client [info]
-        DataInputStream dataDis = null;		// Server -> Client [data]
-        DataOutputStream dataDos = null;	// Client -> Server [data]
-
+        onProgressUpdate(30);
         try {
-            SocketAddress dataSocketAddr = new InetSocketAddress(UploadHostConst.SERVER_IP, UploadHostConst.DATA_PORT);
-            SocketAddress infoSocketAddr = new InetSocketAddress(UploadHostConst.SERVER_IP, UploadHostConst.INFO_PORT);
+            RequestBody requestBody = new MultipartBody.Builder()
+                    .setType(MultipartBody.FORM)
+                    .addFormDataPart("imagefile", "petimage.jpg", RequestBody.create(MultipartBody.FORM, byteArray))
+                    .build();
+            onProgressUpdate(50);
 
-            // 타임 아웃 설정
-            dataSocket.setSoTimeout(UploadHostConst.TIME_OUT);
-            infoSocket.setSoTimeout(UploadHostConst.TIME_OUT);
+            Request request = new Request.Builder()
+                    .url(UploadHostConst.UPLOADER_URL)
+                    .post(requestBody)
+                    .build();
+            onProgressUpdate(60);
 
-            // 서버와 연결한다.
-            dataSocket.connect(dataSocketAddr, UploadHostConst.TIME_OUT);
-            infoSocket.connect(infoSocketAddr, UploadHostConst.TIME_OUT);
+            OkHttpClient client = new OkHttpClient();
+            Call call = client.newCall(request);
+            Response response = call.execute();
+            onProgressUpdate(80);
 
-            infoDos = new DataOutputStream(infoSocket.getOutputStream());
-            infoDis = new DataInputStream(infoSocket.getInputStream());
-            dataDis = new DataInputStream(dataSocket.getInputStream());
-            dataDos = new DataOutputStream(dataSocket.getOutputStream());
+            //System.out.println("Response: " + s);
+            String uploadedName = response.body().string();
 
-            /*
-             * 단일 파일 업로드
-             *
-             * 1. 업로드 할 파일의 크기를 보낸다.
-             * 2. 업로드 가능 여부를 받아온다. false -> reason
-             * 3. 파일 데이터를 보낸다.
-             * 4. URL 값을 받는다.
-             * 5. 스레드 종료.
-             */
-            infoDos.writeInt(fileSize); // 파일 크기 통보
-            boolean canUpload = infoDis.readBoolean();
-            if(!canUpload) {
-                // 업로드 불가, 사유값 읽기
-                String reason = infoDis.readUTF();
-
-                return new Object[]{ false, reason };
+            if (uploadedName.contains("FILE UPLOAD FAILED")) {
+                throw new Exception("Try Later");
             }
-
-            // 업로드 가능, 파일을 업로드 한다.
-            ByteArrayInputStream imageStream = new ByteArrayInputStream(byteArray);
-            byte[] buff = new byte[UploadHostConst.PACKET_SIZE];
-            int readCount = 0;
-            int loaded = 0;
-
-            while((readCount = imageStream.read(buff)) != -1) {
-                dataDos.write(buff, 0, readCount);
-                boolean send = dataDis.readInt() != 0;
-
-                if (!send) {
-                    Log.d("PC", "Packet send failed.");
-                }
-
-                loaded += readCount;
-                onProgressUpdate(loaded);
-            }
-
-            dataDos.flush();
-            dataDis.close();
-            dataDos.close();
-
-            // 파일 업로드 완료.
-            String uploadedName = infoDis.readUTF();
-
             // 성공했습니다.
-            return new Object[]{ true, uploadedName };
-        } catch(SocketException sex) {
+            onProgressUpdate(100);
+            return new Object[]{true, uploadedName};
+        } catch (SocketException sex) {
             // 타임아웃등 연결 실패
             sex.printStackTrace();
-            return new Object[]{ false, sex.getMessage() };
-        } catch(Exception ex) {
+            return new Object[]{false, sex.getMessage()};
+        } catch (Exception ex) {
             // 기타 오류
             ex.printStackTrace();
-            return new Object[]{ false, ex.getMessage() };
+            return new Object[]{false, ex.getMessage()};
         }
     }
 }
--- app/src/main/java/kr/co/aperturedev/petcommunity/modules/uploader/UploadHostConst.java
+++ app/src/main/java/kr/co/aperturedev/petcommunity/modules/uploader/UploadHostConst.java
@@ -7,9 +7,5 @@ package kr.co.aperturedev.petcommunity.modules.uploader;
 public interface UploadHostConst {
     String SERVER_IP = "211.239.124.246";        // Server IP
     String CDN_URL = "http://aperturedev.co.kr:21002/cdn/petcommunity/";
-    int DATA_PORT = 21014;	// 파일 전송 포트
-    int INFO_PORT = 21015;	// 정보 포트
-
-    int TIME_OUT = 20000;    // 타임아웃
-    int PACKET_SIZE = 4096; // 패킷 크기
+    String UPLOADER_URL = "http://aperturedev.co.kr:21002/services/fileupload.php";
 }
