6b58178a40c9ded8c09152922ebe1a6de382bf0d
Party4Bread <solsam10@naver.com>
2018-03-15 23:31:19 +0900

    링크수정

--- app/src/main/java/kr/co/aperturedev/petcommunity/modules/uploader/UploadHostConst.java
+++ app/src/main/java/kr/co/aperturedev/petcommunity/modules/uploader/UploadHostConst.java
@@ -7,5 +7,5 @@ package kr.co.aperturedev.petcommunity.modules.uploader;
 public interface UploadHostConst {
     String SERVER_IP = "211.239.124.246";        // Server IP
     String CDN_URL = "http://aperturedev.co.kr:21002/cdn/petcommunity/";
-    String UPLOADER_URL = "http://aperturedev.co.kr:21002/services/fileupload.php";
+    String UPLOADER_URL = "http://aperturedev.co.kr:21002/petcommunity/services/fileupload.php";
 }
